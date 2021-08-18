7124480ec77e0a9bcc3da1365c4baa8ea3e26229
Michael Moore <mmoore4361@gmail.com>
2021-08-18 14:58:09 +0200

    create server

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const fetch = require('node-fetch');
+let path = require('path');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require(cors());
+app.use(cors());
+
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+    res.sendFile('dist/index.html');
+});
+
+const port = 8081;
+
+app.listen(port, () => {
+    console.log(`Running on localhost: ${port}`);
+});
