3c1bb476c138ca4198ca485666851e90522c29c3
Michael Moore <mmoore4361@gmail.com>
2021-08-19 16:20:20 +0200

    start creating empty functions

---
+++ src/client/js/app.js
@@ -0,0 +1,14 @@
+// const global vars 
+
+
+// getDate function
+
+// getWeatherBit function 
+
+// getGeoName function 
+
+// getPixaBay function 
+
+// updateUI function
+function updateUI() {}
+// Exporting all functions
