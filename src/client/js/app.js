5c743f9057e0d42e92ceebd46f272c6ff360f70e
Michael Moore <mmoore4361@gmail.com>
2021-08-19 16:42:33 +0200

    update index.html

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,8 @@
 // const global vars 
-
+const city = document.getElementById('city');
+const departDate = document.getElementById('departDate');
+const info = document.getElementById('info');
+const temp = document.getElementById('temp');
 
 // getDate function
 
