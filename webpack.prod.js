235a620fd00b9d0320c393bde5a442821b8d8cf6
Michael Moore <mmoore4361@gmail.com>
2021-08-19 06:34:56 +0200

    remove service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin')
+// const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -34,7 +34,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new WorkboxPlugin.GenerateSW(),
+        // new WorkboxPlugin.GenerateSW(),
         new MiniCssExtractPlugin({ filename: '[name].css' }),
         new CleanWebpackPlugin({
             dry: true,
