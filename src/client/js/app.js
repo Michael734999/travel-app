24311433e7aae881d3e508d905830eb8de9a1144
Michael Moore <mmoore4361@gmail.com>
2021-08-23 12:04:26 +0200

    fix errors

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    Client.showSaved();
+    showSaved();
 });
 // getWeatherBit function 
 const getWeatherBit = async(daysLeft, lat, long) => {
