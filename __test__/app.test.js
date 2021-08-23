752628dd59a00317af985363e3e913ff9d388af1
Michael Moore <mmoore4361@gmail.com>
2021-08-23 15:34:35 +0200

    update jest

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,4 +1,4 @@
-import { handleSubmit } from "../src/client";
+import { handleSubmit } from "../src/client/js/app";
 
 describe('Check Handlesubmit function', () => {
     test('Check handlesubmit function', () => {
